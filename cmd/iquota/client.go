6a6fdd103f30d85deebbd5988f433026621dcb46
Andrew E. Bruno <aebruno2@buffalo.edu>
2015-12-12 22:57:37 -0500

    Add config option for specifiying which quotas to report on.
    - This option allows for configuring iquota to only report on specific
      filesystems found in /etc/mtab. By default all /ifs nfs mounted
      filesystems are reported on.
    
    - Additionally, for each filesystem in the config file you can specifiy
      which quotas to report (user, group or both)

--- cmd/iquota/client.go
+++ cmd/iquota/client.go
@@ -45,6 +45,8 @@ type Filesystem struct {
 	Host       string
 	Path       string
 	MountPoint string
+    UserQuota  bool
+    GroupQuota bool
 }
 
 type QuotaClient struct {
@@ -139,6 +141,8 @@ func (c *QuotaClient) fetchQuota(url string) (*iquota.QuotaRestResponse, error) 
 func (c *QuotaClient) parseMtab() ([]*Filesystem, error) {
 	mounts := make([]*Filesystem, 0)
 
+    defaultFs := viper.GetStringMapString("filesystems")
+
 	mtab, err := os.Open("/etc/mtab")
 	if err != nil {
 		return nil, err
@@ -150,9 +154,21 @@ func (c *QuotaClient) parseMtab() ([]*Filesystem, error) {
 		fields := strings.Split(scanner.Text(), " ")
 		if fields[2] == "nfs" {
 			parts := strings.Split(fields[0], ":")
-			// XXX only include isilon mounts. Will this always be /ifs?
-			if strings.HasPrefix(parts[1], "/ifs") {
-				fs := &Filesystem{Host: parts[0], Path: parts[1], MountPoint: fields[1]}
+            fs := &Filesystem{
+                Host: parts[0],
+                Path: parts[1],
+                MountPoint: fields[1],
+                UserQuota: true,
+                GroupQuota: true,
+            }
+
+            defaults, ok := defaultFs[fs.Path]
+            if ok {
+                fs.UserQuota = strings.Contains(defaults, "user")
+                fs.GroupQuota = strings.Contains(defaults, "group")
+				mounts = append(mounts, fs)
+            } else if len(defaultFs) == 0 && strings.HasPrefix(fs.Path, "/ifs") {
+                // XXX only include isilon mounts. Will this always be /ifs?
 				mounts = append(mounts, fs)
 			}
 		}
@@ -246,6 +262,10 @@ func (c *QuotaClient) printUserQuota(username string, mounts []*Filesystem) {
 	fmt.Printf("User quotas:\n")
 	c.printHeader("user")
 	for _, fs := range mounts {
+        if !fs.UserQuota {
+            logrus.Warn("User quota reporting disabled for filesystem: ", fs)
+            continue
+        }
 		params := url.Values{}
 		params.Add("user", username)
 		params.Add("path", fs.Path)
@@ -297,6 +317,10 @@ func (c *QuotaClient) printGroupQuota(username string, mounts []*Filesystem) {
 	group := c.GroupFilter
 
 	for _, fs := range mounts {
+        if !fs.GroupQuota {
+            logrus.Warn("Group quota reporting disabled for filesystem: ", fs)
+            continue
+        }
 		params := url.Values{}
 		params.Add("user", username)
 		params.Add("path", fs.Path)
