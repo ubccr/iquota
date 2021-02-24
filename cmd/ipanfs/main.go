b7afe253ac252b267603dcafc03e049f54b97ea1
Andrew E. Bruno <aebruno2@buffalo.edu>
2021-02-23 21:19:13 -0500

    Refactor panfs caching.
    We now use volume quotas. This commit uses the panfs XML API to fetch
    all volume quotas to cache. We also set the file indode limits using
    user/group quotas. As a simple hack we just set the used inodes for a
    volume to be that of the largest group.

--- cmd/ipanfs/main.go
+++ cmd/ipanfs/main.go
@@ -8,14 +8,18 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"crypto/tls"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
 	"os"
-	"os/user"
 	"strconv"
 	"strings"
 
+	"github.com/dustin/go-humanize"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/ubccr/iquota"
@@ -23,30 +27,123 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
+type PanfsRoot struct {
+	Pasroot xml.Name  `xml:"pasxml"`
+	Volumes []*Volume `xml:"volumes>volume"`
+}
+
+type Volume struct {
+	Name string `xml:"name"`
+	Used string `xml:"spaceUsedGB"`
+	Hard string `xml:"hardQuotaGB"`
+	Soft string `xml:"softQuotaGB"`
+}
+
 var (
-	PanfsQuotaCmd = "userquota usage -output tab"
-	SkipPaths     = []string{"/home"}
-	SkipUsers     = []string{"root"}
-	SkipGroups    = []string{"wheel"}
+	PanfsQuotaCmd        = "userquota usage -output tab"
+	PanfsLoginEndpoint   = "/pasxml/login"
+	PanfsVolumesEndpoint = "/pasxml/volumes"
+	client               *http.Client
+
+	prefix = kingpin.Flag(
+		"prefix",
+		"Path prefix for mount point of panfs",
+	).Default("/panasas").Envar("IPANFS_PREFIX").String()
+
+	address = kingpin.Flag(
+		"address",
+		"Address of panfs server",
+	).Required().Envar("IPANFS_ADDRESS").String()
+
+	panUser = kingpin.Flag(
+		"user",
+		"SSH user",
+	).Default("guest").Envar("IPANFS_USER").String()
+
+	panPass = kingpin.Flag(
+		"password",
+		"SSH password",
+	).Envar("IPANFS_PASSWORD").String()
+
+	key = kingpin.Flag(
+		"key",
+		"SSH key",
+	).Envar("IPANFS_KEY").String()
+
+	expire = kingpin.Flag(
+		"expire",
+		"Cache expire time",
+	).Default("500").Envar("IPANFS_EXPIRE").Int()
+
+	debug = kingpin.Flag("debug", "enable debug mode").Default("false").Bool()
+	noop  = kingpin.Flag("noop", "Dump quota report from panfs and exit").Default("false").Bool()
 )
 
 func init() {
 	viper.SetConfigName("iquota")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("/etc/iquota/")
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatalf("Failed to create cookie jar: %s", err)
+	}
+	client = &http.Client{Jar: jar, Transport: tr}
 }
 
-func skip(list []string, name string) bool {
-	for _, x := range list {
-		if x == name {
-			return true
-		}
+func login() error {
+	req, err := http.NewRequest("GET", "https://"+*address+":10635/"+PanfsLoginEndpoint, nil)
+
+	params := req.URL.Query()
+	params.Add("name", *panUser)
+	params.Add("pass", *panPass)
+	req.URL.RawQuery = params.Encode()
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("Failed to login to panfs with HTTP status code: %d", res.StatusCode)
 	}
 
-	return false
+	return nil
 }
 
-func cacheGroupQuota(cache *iquota.Cache, prefix string, report io.Reader) error {
+func fetchVolumes() ([]*Volume, error) {
+	err := login()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", "https://"+*address+":10635/"+PanfsVolumesEndpoint, nil)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Failed to login to panfs with HTTP status code: %d", res.StatusCode)
+	}
+
+	rawXml, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result PanfsRoot
+	err = xml.Unmarshal(rawXml, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Volumes, nil
+}
+
+func parseGroupQuotas(report io.Reader) (map[string]*iquota.Quota, error) {
 	scanner := bufio.NewScanner(report)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -55,6 +152,7 @@ func cacheGroupQuota(cache *iquota.Cache, prefix string, report io.Reader) error
 		}
 	}
 
+	quotas := make(map[string]*iquota.Quota, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" || strings.HasPrefix(line, "#") {
@@ -62,140 +160,36 @@ func cacheGroupQuota(cache *iquota.Cache, prefix string, report io.Reader) error
 		}
 
 		cols := strings.Split(scanner.Text(), "\t")
-		path := fmt.Sprintf("%s%s", prefix, cols[0])
-		if skip(SkipPaths, path) {
-			continue
-		}
-
-		groupname := cols[3]
-		log.Debugf("Found groupname: %s", groupname)
-		if strings.HasPrefix(groupname, "gid:") {
-			gid := strings.ReplaceAll(groupname, "gid:", "")
-			g, err := user.LookupGroupId(gid)
-			if err != nil {
-				groupname = gid
-				log.WithFields(log.Fields{
-					"path":  path,
-					"group": groupname,
-					"error": err,
-				}).Error("Failed to lookup gidnumber")
-			} else {
-				groupname = g.Name
-			}
-		}
-		if skip(SkipGroups, groupname) {
-			continue
-		}
-
-		bytesUsed, _ := strconv.Atoi(cols[5])
-		soft, _ := strconv.Atoi(cols[6])
-		//softPct, _ := strconv.Atoi(cols[7])
-		hard, _ := strconv.Atoi(cols[8])
-		//hardPct, _ := strconv.Atoi(cols[9])
+		path := cols[0]
 		filesUsed, _ := strconv.Atoi(cols[10])
 
-		quota := &iquota.Quota{
-			Path:       path,
-			HardLimit:  hard,
-			SoftLimit:  soft,
-			Used:       bytesUsed,
-			UsedInodes: filesUsed,
-		}
-
-		err := cache.SetDirectoryQuotaCache(path, quota)
-		if err != nil {
-			log.WithFields(log.Fields{
-				"path":  path,
-				"error": err,
-			}).Error("Failed to set panfs directory quota cache in redis")
-		}
-	}
-
-	return nil
-}
-
-func cacheUserQuota(cache *iquota.Cache, prefix string, report io.Reader) error {
-	scanner := bufio.NewScanner(report)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "# User Quota Usage" {
-			break
-		}
-	}
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" || strings.HasPrefix(line, "#") {
-			if line == "# Group Quota Usage" {
-				break
+		q, ok := quotas[path]
+		if ok {
+			if filesUsed > q.UsedInodes {
+				// If multiple groups just take the largest one
+				quotas[path].UsedInodes = filesUsed
 			}
-			continue
-		}
-
-		cols := strings.Split(scanner.Text(), "\t")
-		path := fmt.Sprintf("%s%s", prefix, cols[0])
-		if skip(SkipPaths, path) {
-			continue
-		}
-
-		username := cols[3]
-		log.Debugf("Found username: %s", username)
-
-		if strings.HasPrefix(username, "uid:") {
-			uid := strings.ReplaceAll(username, "uid:", "")
-			u, err := user.LookupId(uid)
-			if err != nil {
-				username = uid
-				log.WithFields(log.Fields{
-					"path":  path,
-					"user":  username,
-					"error": err,
-				}).Error("Failed to lookup uidnumber")
-			} else {
-				username = u.Username
+		} else {
+			quotas[path] = &iquota.Quota{
+				Path:       path,
+				UsedInodes: filesUsed,
 			}
 		}
-		if skip(SkipUsers, username) {
-			continue
-		}
-
-		bytesUsed, _ := strconv.Atoi(cols[5])
-		soft, _ := strconv.Atoi(cols[6])
-		//softPct, _ := strconv.Atoi(cols[7])
-		hard, _ := strconv.Atoi(cols[8])
-		//hardPct, _ := strconv.Atoi(cols[9])
-		filesUsed, _ := strconv.Atoi(cols[10])
-
-		quota := &iquota.Quota{
-			Path:       path,
-			HardLimit:  hard,
-			SoftLimit:  soft,
-			Used:       bytesUsed,
-			UsedInodes: filesUsed,
-		}
-
-		err := cache.SetDirectoryQuotaCache(path, quota)
-		if err != nil {
-			log.WithFields(log.Fields{
-				"path":  path,
-				"error": err,
-			}).Error("Failed to set panfs directory quota cache in redis")
-		}
 	}
 
-	return nil
+	return quotas, nil
 }
 
-func fetchQuotaReport(address, username, pass, key string) (*bytes.Buffer, error) {
+func runPanfsCmd(command string) (*bytes.Buffer, error) {
 	authMethods := make([]ssh.AuthMethod, 0)
 
-	if key != "" {
-		key, err := ioutil.ReadFile(key)
+	if *key != "" {
+		sshKey, err := ioutil.ReadFile(*key)
 		if err != nil {
 			return nil, err
 		}
 
-		signer, err := ssh.ParsePrivateKey(key)
+		signer, err := ssh.ParsePrivateKey(sshKey)
 		if err != nil {
 			return nil, err
 		}
@@ -203,17 +197,17 @@ func fetchQuotaReport(address, username, pass, key string) (*bytes.Buffer, error
 		authMethods = append(authMethods, ssh.PublicKeys(signer))
 	}
 
-	if pass != "" {
-		authMethods = append(authMethods, ssh.Password(pass))
+	if *panPass != "" {
+		authMethods = append(authMethods, ssh.Password(*panPass))
 	}
 
 	config := &ssh.ClientConfig{
-		User:            username,
+		User:            *panUser,
 		Auth:            authMethods,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
-	conn, err := ssh.Dial("tcp", address, config)
+	conn, err := ssh.Dial("tcp", *address+":22", config)
 	if err != nil {
 		return nil, err
 	}
@@ -230,51 +224,21 @@ func fetchQuotaReport(address, username, pass, key string) (*bytes.Buffer, error
 	sess.Stdout = &stdout
 	sess.Stderr = &stderr
 
-	err = sess.Run(PanfsQuotaCmd)
+	err = sess.Run(command)
 	if err != nil {
 		log.Debugf("Failed to run userquota: %s", stderr.String())
 		return nil, err
 	}
 
 	return &stdout, nil
+
+}
+
+func fetchQuotaReport() (*bytes.Buffer, error) {
+	return runPanfsCmd(PanfsQuotaCmd)
 }
 
 func main() {
-	var (
-		prefix = kingpin.Flag(
-			"prefix",
-			"Path prefix for mount point of panfs",
-		).Default("/panasas").Envar("IPANFS_PREFIX").String()
-
-		address = kingpin.Flag(
-			"address",
-			"Address of panfs server [host:port]",
-		).Required().Envar("IPANFS_ADDRESS").String()
-
-		panUser = kingpin.Flag(
-			"user",
-			"SSH user",
-		).Default("guest").Envar("IPANFS_USER").String()
-
-		panPass = kingpin.Flag(
-			"password",
-			"SSH password",
-		).Envar("IPANFS_PASSWORD").String()
-
-		key = kingpin.Flag(
-			"key",
-			"SSH key",
-		).Envar("IPANFS_KEY").String()
-
-		expire = kingpin.Flag(
-			"expire",
-			"Cache expire time",
-		).Default("500").Envar("IPANFS_EXPIRE").Int()
-
-		debug = kingpin.Flag("debug", "enable debug mode").Default("false").Bool()
-		noop  = kingpin.Flag("noop", "Dump quota report from panfs and exit").Default("false").Bool()
-	)
-
 	viper.ReadInConfig()
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
@@ -285,29 +249,65 @@ func main() {
 		log.SetLevel(log.WarnLevel)
 	}
 
-	out, err := fetchQuotaReport(*address, *panUser, *panPass, *key)
+	report, err := fetchQuotaReport()
 	if err != nil {
 		log.Fatalf("Failed to fetch quota report from panfs: %s", err)
 	}
 
+	volumes, err := fetchVolumes()
+	if err != nil {
+		log.Fatalf("Failed to fetch volumes: %s", err)
+	}
 	if *noop {
-		fmt.Println(out.String())
+		for _, v := range volumes {
+			fmt.Printf("%#v\n", v)
+		}
+		fmt.Println(report.String())
 		os.Exit(0)
 	}
 
-	cache := &iquota.Cache{Expire: *expire}
-
-	reader := bytes.NewReader(out.Bytes())
-
-	err = cacheUserQuota(cache, *prefix, reader)
-	if err != nil {
-		log.Fatalf("Failed to parse user quota report from panfs: %s", err)
-	}
-
-	reader.Seek(0, 0)
-
-	err = cacheGroupQuota(cache, *prefix, reader)
+	reader := bytes.NewReader(report.Bytes())
+	gquotas, err := parseGroupQuotas(reader)
 	if err != nil {
 		log.Fatalf("Failed to parse group quota report from panfs: %s", err)
+	}
+
+	cache := &iquota.Cache{Expire: *expire}
+	for _, v := range volumes {
+		path := fmt.Sprintf("%s%s", *prefix, v.Name)
+
+		hard, err := humanize.ParseBytes(v.Hard + " GB")
+		if err != nil {
+			log.Errorf("Failed to parse hard quota to bytes for path %s: %s", v.Name, err)
+		}
+		soft, err := humanize.ParseBytes(v.Soft + " GB")
+		if err != nil {
+			log.Errorf("Failed to parse soft quota to bytes for path %s: %s", v.Name, err)
+		}
+		used, err := humanize.ParseBytes(v.Used + " GB")
+		if err != nil {
+			log.Errorf("Failed to parse used bytes for path %s: %s", v.Name, err)
+		}
+		iq := &iquota.Quota{
+			Path:        path,
+			GracePeriod: "7 days",
+			HardLimit:   int(hard),
+			SoftLimit:   int(soft),
+			Used:        int(used),
+		}
+
+		q, ok := gquotas[v.Name]
+		if ok {
+			// XXX hack to set the number of files used from the group quota
+			iq.UsedInodes = q.UsedInodes
+		}
+
+		err = cache.SetDirectoryQuotaCache(path, iq)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"path":  path,
+				"error": err,
+			}).Error("Failed to set panfs directory quota cache in redis")
+		}
 	}
 }
