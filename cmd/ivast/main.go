77e37be419c439ecbe8ff888ad2f2a78aa9fecf8
Andrew E. Bruno <aebruno2@buffalo.edu>
2021-03-11 08:05:53 -0500

    Add setting vast quotas.
    This commit add functionality to ivast to get/set quotas. This tool is
    used to manage vast quotas until we implement a more robust solution.

--- cmd/ivast/main.go
+++ cmd/ivast/main.go
@@ -7,8 +7,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"path"
+	"strings"
 
+	"github.com/dustin/go-humanize"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/ubccr/iquota"
@@ -47,6 +50,59 @@ import (
    },
 */
 
+const (
+	LongFormat = "%-30s%15s%15s%10s%10s%12s\n"
+)
+
+var (
+	DefaultUserQuotaLimit = uint64(10000000000)   //  10G
+	DefaultUserFilesLimit = uint64(10000000)      //  10M
+	DefaultQuotaLimit     = uint64(1000000000000) //   1T
+	DefaultFilesLimit     = uint64(200000000)     // 200M
+
+	debug = kingpin.Flag("debug", "enable debug mode").Default("false").Bool()
+
+	vastHost = kingpin.Flag(
+		"host",
+		"Hostname of vast server",
+	).Default("vast-mgt.cbls.ccr.buffalo.edu").Envar("VAST_HOST").String()
+
+	vastUser = kingpin.Flag(
+		"user",
+		"Vast user",
+	).Default("quota-reporter").Envar("VAST_USER").String()
+
+	vastPass = kingpin.Flag(
+		"password",
+		"Vast password",
+	).Envar("VAST_PASSWORD").String()
+
+	cmdGetQuota  = kingpin.Command("get-quota", "Get VAST quota")
+	getQuotaPath = cmdGetQuota.Flag(
+		"path",
+		"Path to directory",
+	).Required().String()
+
+	cmdSetQuota = kingpin.Command("set-quota", "Set VAST quota")
+	quotaPath   = cmdSetQuota.Flag(
+		"path",
+		"Path to directory for setting quota",
+	).Required().String()
+
+	quotaLimit = cmdSetQuota.Flag(
+		"limit",
+		"The quota size limit",
+	).String()
+
+	cmdCache = kingpin.Command("cache", "Cache VAST quotas")
+	expire   = cmdCache.Flag(
+		"expire",
+		"Cache expire time",
+	).Default("500").Envar("VAST_EXPIRE").Int()
+
+	cmdUserCheck = kingpin.Command("user-check", "Check and set user home directories")
+)
+
 type vastQuota struct {
 	ID                    int    `json:"id"`
 	HardLimit             int    `json:"hard_limit"`
@@ -68,13 +124,19 @@ func init() {
 	viper.AddConfigPath("/etc/iquota/")
 }
 
-func fetchQuotaReport(host, user, password string) ([]vastQuota, error) {
+func fetchQuotaReport(dirPath string) ([]vastQuota, error) {
 	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
 	client := &http.Client{Transport: tr}
 
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s/api/quotas/", host), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s/api/quotas/", *vastHost), nil)
+	if len(dirPath) > 0 {
+		params := req.URL.Query()
+		params.Add("path", dirPath)
+		req.URL.RawQuery = params.Encode()
+	}
+
 	req.Header.Set("Accept", "application/json")
-	req.SetBasicAuth(user, password)
+	req.SetBasicAuth(*vastUser, *vastPass)
 	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
@@ -99,7 +161,17 @@ func fetchQuotaReport(host, user, password string) ([]vastQuota, error) {
 	return vq, nil
 }
 
-func setDefaultUserQuota(path, host, user, password string) error {
+func createVastDirectoryQuota(path string, softLimit, filesLimit uint64) error {
+	apiUrl := fmt.Sprintf("https://%s/api/quotas/", *vastHost)
+	return setVastDirectoryQuota("POST", apiUrl, path, softLimit, filesLimit)
+}
+
+func updateVastDirectoryQuota(quota vastQuota, path string, softLimit, filesLimit uint64) error {
+	apiUrl := fmt.Sprintf("https://%s/api/quotas/%d/", *vastHost, quota.ID)
+	return setVastDirectoryQuota("PATCH", apiUrl, path, softLimit, filesLimit)
+}
+
+func setVastDirectoryQuota(verb, apiUrl, path string, softLimit, filesLimit uint64) error {
 	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
 	client := &http.Client{Transport: tr}
 
@@ -107,10 +179,10 @@ func setDefaultUserQuota(path, host, user, password string) error {
 		"name":              path,
 		"path":              path,
 		"grace_period":      "7 00:00:00",
-		"soft_limit":        10000000000,
-		"hard_limit":        11000000000,
-		"soft_limit_inodes": 10000000,
-		"hard_limit_inodes": 10100000,
+		"soft_limit":        softLimit,
+		"hard_limit":        softLimit + 1000000000, // add 1G
+		"soft_limit_inodes": filesLimit,
+		"hard_limit_inodes": filesLimit + 100000, // add 100K
 		"create_dir":        "False",
 	}
 
@@ -119,36 +191,90 @@ func setDefaultUserQuota(path, host, user, password string) error {
 		return err
 	}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/api/quotas/", host), bytes.NewBuffer(b))
+	req, err := http.NewRequest(verb, apiUrl, bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(user, password)
+	req.SetBasicAuth(*vastUser, *vastPass)
 	res, err := client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
 
-	if res.StatusCode != 201 {
-		rawBody, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			return err
-		}
-
-		return fmt.Errorf("Failed to create vast quota for path %s: %s", path, string(rawBody))
+	if res.StatusCode == 201 || res.StatusCode == 200 {
+		return nil
 	}
 
-	return nil
+	rawBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	return fmt.Errorf("Failed with code %d createing vast quota for path %s: %s", res.StatusCode, path, string(rawBody))
 }
 
-func checkAndSetUserQuotas(host, user, password string) {
+func setDirectoryQuota() {
+	dirPath := strings.TrimSuffix(*quotaPath, "/")
+	if len(dirPath) == 0 {
+		log.Fatalf("Please provide a directory path (--path)")
+	}
+
+	if !strings.HasPrefix(dirPath, "/") {
+		log.Fatalf("Paths must be absolute: %s", *quotaPath)
+	}
+
+	finfo, err := os.Stat(dirPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("Directory does not exist: %s", *quotaPath)
+		}
+
+		log.Fatalf("Failed to check directory path %s: %s", *quotaPath, err)
+	}
+
+	if !finfo.IsDir() {
+		log.Fatalf("Path exist but is not a directory: %s", *quotaPath)
+	}
+
+	bytes := DefaultQuotaLimit
+	if len(*quotaLimit) > 0 {
+		bytes, err = humanize.ParseBytes(*quotaLimit)
+		if err != nil {
+			log.Fatalf("Invalid quota limit value: %s", err)
+		}
+	}
+
+	quotas, err := fetchQuotaReport(dirPath)
+	if err != nil {
+		log.Fatalf("Failed to check quota report for %s: %s", dirPath, err)
+	}
+
+	if len(quotas) == 0 {
+		err = createVastDirectoryQuota(dirPath, bytes, DefaultFilesLimit)
+	} else if len(quotas) == 1 {
+		err = updateVastDirectoryQuota(quotas[0], dirPath, bytes, DefaultFilesLimit)
+	} else {
+		log.Fatalf("Failed to set quota. More than one already exists: ", dirPath)
+	}
+
+	if err != nil {
+		log.Fatalf("Failed to set quota on %s: %s", dirPath, err)
+	}
+
+	fmt.Printf("Successfully set %s quota on %s\n", humanize.Bytes(bytes), dirPath)
+}
+
+func checkAndSetUserQuotas() {
 	if len(viper.GetString("home_dir")) == 0 {
 		log.Fatalf("You must set the home_dir config value")
 	}
 
 	log.Infof("Checking all directories under: %s", viper.GetString("home_dir"))
 
-	quotas, err := fetchQuotaReport(host, user, password)
+	quotas, err := fetchQuotaReport("")
 	if err != nil {
 		log.Fatalf("Failed to fetch all quotas from vast: %s", err)
 	}
@@ -183,7 +309,7 @@ func checkAndSetUserQuotas(host, user, password string) {
 		}
 
 		log.Infof("Setting new user quota on path: %s", abspath)
-		err := setDefaultUserQuota(abspath, host, user, password)
+		err := createVastDirectoryQuota(abspath, DefaultUserQuotaLimit, DefaultUserFilesLimit)
 		if err != nil {
 			log.Errorf("%s", err)
 		} else {
@@ -198,53 +324,40 @@ func checkAndSetUserQuotas(host, user, password string) {
 	}
 }
 
-func main() {
-	var (
-		host = kingpin.Flag(
-			"host",
-			"Hostname of vast server",
-		).Required().Envar("VAST_HOST").String()
-
-		vastUser = kingpin.Flag(
-			"user",
-			"Vast user",
-		).Default("quota-reporter").Envar("VAST_USER").String()
-
-		vastPass = kingpin.Flag(
-			"password",
-			"Vast password",
-		).Envar("VAST_PASSWORD").String()
-
-		expire = kingpin.Flag(
-			"expire",
-			"Cache expire time",
-		).Default("500").Envar("VAST_EXPIRE").Int()
-
-		debug     = kingpin.Flag("debug", "enable debug mode").Default("false").Bool()
-		userCheck = kingpin.Flag("user-check", "check and set user home directories").Default("false").Bool()
-	)
-
-	viper.ReadInConfig()
-	kingpin.HelpFlag.Short('h')
-	kingpin.Parse()
-
-	log.SetFormatter(&log.TextFormatter{TimestampFormat: "2006-01-02 15:04:05", FullTimestamp: true})
-
-	if *debug {
-		log.SetLevel(log.DebugLevel)
-	} else {
-		log.SetLevel(log.WarnLevel)
+func getDirectoryQuota() {
+	dirPath := strings.TrimSuffix(*getQuotaPath, "/")
+	if len(dirPath) == 0 {
+		log.Fatalf("Please provide a directory path (--path)")
 	}
 
-	if *userCheck {
-		checkAndSetUserQuotas(*host, *vastUser, *vastPass)
-		return
+	if !strings.HasPrefix(dirPath, "/") {
+		log.Fatalf("Paths must be absolute: %s", *quotaPath)
 	}
 
-	quotas, err := fetchQuotaReport(*host, *vastUser, *vastPass)
+	quotas, err := fetchQuotaReport(dirPath)
+	if err != nil {
+		log.Fatalf("Failed to fetch quota report for %s: %s", dirPath, err)
+	}
+
+	fmt.Printf(LongFormat, "Path ", "files", "used", "soft", "hard", "grace ")
+	for _, q := range quotas {
+		fmt.Printf(LongFormat,
+			q.Path,
+			humanize.Comma(int64(q.UsedInodes)),
+			humanize.Bytes(uint64(q.UsedEffectiveCapacity)),
+			humanize.Bytes(uint64(q.SoftLimit)),
+			humanize.Bytes(uint64(q.HardLimit)),
+			q.GracePeriod)
+	}
+}
+
+func cacheQuotas() {
+	quotas, err := fetchQuotaReport("")
 	if err != nil {
 		log.Fatalf("Failed to fetch quota report from vast: %s", err)
 	}
+
+	log.Infof("Found %d quotas from vast", len(quotas))
 
 	cache := &iquota.Cache{Expire: *expire}
 
@@ -267,5 +380,36 @@ func main() {
 				"error": err,
 			}).Error("Failed to set vast directory quota cache in redis")
 		}
+
+		log.Infof("Successfully cached %s quota for %s", humanize.Bytes(uint64(q.SoftLimit)), q.Path)
+	}
+}
+
+func main() {
+	viper.ReadInConfig()
+	kingpin.HelpFlag.Short('h')
+	cmd := kingpin.Parse()
+
+	log.SetFormatter(&log.TextFormatter{TimestampFormat: "2006-01-02 15:04:05", FullTimestamp: true})
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.WarnLevel)
+	}
+
+	switch cmd {
+	case cmdUserCheck.FullCommand():
+		checkAndSetUserQuotas()
+
+	case cmdSetQuota.FullCommand():
+		setDirectoryQuota()
+
+	case cmdGetQuota.FullCommand():
+		getDirectoryQuota()
+
+	case cmdCache.FullCommand():
+		cacheQuotas()
+	default:
+		kingpin.Usage()
 	}
 }
